48ab97901595f5b092cf2c28910890244638ca48
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-08-16 17:22:05 +0200

    Make the benchmarks more similar

--- benchmark_test.go
+++ benchmark_test.go
@@ -13,45 +13,54 @@ import (
 	"github.com/russross/blackfriday"
 )
 
-func BenchmarkGoldMark(b *testing.B) {
-	b.ResetTimer()
-	source, err := ioutil.ReadFile("_data.md")
-	if err != nil {
-		panic(err)
-	}
-	markdown := goldmark.New(
-		goldmark.WithRendererOptions(html.WithXHTML(), html.WithUnsafe()),
-	)
-	var out bytes.Buffer
-	markdown.Convert([]byte(""), &out)
-
-	for i := 0; i < b.N; i++ {
-		out.Reset()
-		if err := markdown.Convert(source, &out); err != nil {
-			panic(err)
+func BenchmarkMarkdown(b *testing.B) {
+	b.Run("Blackfriday-v2", func(b *testing.B) {
+		r := func(src []byte) ([]byte, error) {
+			out := blackfriday.Run(src)
+			return out, nil
 		}
-	}
+		doBenchmark(b, r)
+	})
+
+	b.Run("GoldMark", func(b *testing.B) {
+		markdown := goldmark.New(
+			goldmark.WithRendererOptions(html.WithXHTML(), html.WithUnsafe()),
+		)
+		r := func(src []byte) ([]byte, error) {
+			var out bytes.Buffer
+			err := markdown.Convert(src, &out)
+			return out.Bytes(), err
+		}
+		doBenchmark(b, r)
+	})
+
+	b.Run("CommonMark", func(b *testing.B) {
+		md := markdown.New(markdown.XHTMLOutput(true))
+		r := func(src []byte) ([]byte, error) {
+			var out bytes.Buffer
+			err := md.Render(&out, src)
+			return out.Bytes(), err
+		}
+		doBenchmark(b, r)
+	})
 }
 
-func BenchmarkGolangCommonMark(b *testing.B) {
-	b.ResetTimer()
+// The different frameworks have different APIs. Create an adapter that
+// should behave the same in the memory department.
+func doBenchmark(b *testing.B, render func(src []byte) ([]byte, error)) {
+	b.StopTimer()
 	source, err := ioutil.ReadFile("_data.md")
 	if err != nil {
-		panic(err)
+		b.Fatal(err)
 	}
-	md := markdown.New(markdown.XHTMLOutput(true))
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		md.RenderToString(source)
-	}
-}
-
-func BenchmarkBlackFriday(b *testing.B) {
-	b.ResetTimer()
-	source, err := ioutil.ReadFile("_data.md")
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < b.N; i++ {
-		blackfriday.Run(source)
+		out, err := render(source)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if len(out) < 100 {
+			b.Fatal("No result")
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/russross/blackfriday v2.0.0+incompatible
+	github.com/russross/blackfriday/v2 v2.0.1
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/yuin/goldmark v1.0.9
 	gitlab.com/golang-commonmark/html v0.0.0-20180917080848-cfaf75183c4a // indirect
